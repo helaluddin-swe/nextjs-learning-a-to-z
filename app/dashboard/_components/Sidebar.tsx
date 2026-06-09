28cfd6ea2111220f914945f59c819609e0dafc35
Helal Uddin -SWE <seoguro1999@gmail.com>
2026-06-09 06:10:06 +0600

    add dashbaord

--- app/dashboard/_components/Sidebar.tsx
+++ app/dashboard/_components/Sidebar.tsx
@@ -1,14 +1,18 @@
+"use client"
 import Link from "next/link"
+import { usePathname } from "next/navigation"
 
 const Sidebar = () => {
+  const pathname=usePathname()
+
   return (
     <div className="border-r-2 border-gray-500 bg-gray-300 h-screen p-4 ">
       <ul className="flex flex-col gap-2">
         <li>
-      <Link href="/dashboard/customers">Customer</Link>
+      <Link href="/dashboard/customers" className={`${pathname==="/dashboard/customers" ?"text-red-500":""}`}>Customer</Link>
         </li>
         <li>
-      <Link href="/dashboard/invoices">Invoices</Link>
+      <Link href="/dashboard/invoices" className={`${pathname==="/dashboard/invoices" ?"text-red-500":""}`}>Invoices</Link>
         </li>
         </ul>
     </div>
